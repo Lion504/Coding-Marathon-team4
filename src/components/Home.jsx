b6e7abf5015a07fa9d2abbca1af554d06ad1105a
Unai San Segundo <unais@metropolia.fi>
2025-11-14 15:03:31 +0200

    feat: add names to homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
 function Home() {
   return (
     <div>
-      <h2>Group # ?</h2>
+      <h2>Group #4</h2>
       <p>Code Marathon 1</p>
+      <p>by: Kumudu, Tamseela, Taysa, Unai, Wang</p>
+      <ol>
+        <li>Book Collection Manager - Taysa</li>
+        <li>Contact List Manager - Wang</li>
+        <li>Recipe Manager - Kumudu</li>
+        <li>Shopping Cart - Unai</li>
+        <li>Signup Page - Tamseela</li>
+      </ol>
     </div>
   );
 }
