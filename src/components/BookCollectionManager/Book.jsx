cfdad8941358cc5719251debf2bf3e0c819d2616
Taysa Abinader <ferreira.taysa@gmail.com>
2025-11-14 13:14:46 +0200

    fix(book): some bug fixes.

--- src/components/BookCollectionManager/Book.jsx
+++ src/components/BookCollectionManager/Book.jsx
@@ -4,12 +4,17 @@ function Book (props) {
     const { book, onDelete } = props;
 
     return (
-        <li>
-        <strong>{book.title}</strong> by {book.author}
-            {" – "}
-            {book.genre}, {book.language}, {book.edition},{" "}
-            {book.pages} pages, rating: {book.rating}
-            <button onClick={onDelete}>Delete</button>
+        <li className="book-item">
+            <div className="book-info">
+                <div className="book-title"><strong>{book.title}</strong> by {book.author}</div>
+                <div className="book-details">
+                    {" – "}
+                    {book.genre}, {book.language}, {book.edition},{" "}
+                    {book.pages} pages, rating: {book.rating}
+                </div>
+            </div>
+            <button className="delete-button" onClick={onDelete}>Delete</button>
+
         </li>
         );
 }
